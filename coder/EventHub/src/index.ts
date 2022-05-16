17e955b22ec2a71ebbb67776db911d6a4088b5cb
liuyidiao <1070692044@qq.com>
2022-05-16 21:06:43 +0800

    优化EventHub

--- coder/EventHub/src/index.ts
+++ coder/EventHub/src/index.ts
@@ -1,9 +1,9 @@
 //新建EventHub类并导出   发布订阅模式
 class EventHub {
-    cache = {}  // 用来缓存 {'bili':[fn1,fn2,fn3],'git':[fn1,fn2,fn3]}
+    private cache: {[key:string]:Array<(data:unknown)=>void>}  // 用来缓存 {'bili':[fn1,fn2,fn3],'git':[fn1,fn2,fn3]}
 
     //发布  参数1.事件名  2.处理函数
-    on(eventName, fn) {
+    on(eventName:string, fn:(data:unknown)=>void) {
         //把fn推进到缓存数组
         // if (this.cache[eventName] === undefined) {
         //     this.cache[eventName].push(fn);
@@ -12,8 +12,8 @@ class EventHub {
         this.cache[eventName].push(fn)
     }
 
-    //订阅
-    emit(eventName) {
+    //订阅   data可以为空: ？
+    emit(eventName, data?:unknown) {
         //把缓存数组的fn依次调用
         // let array = this.cache[eventName]
         // if (array === undefined) {
@@ -23,9 +23,46 @@ class EventHub {
         // array.forEach(fn => {
         //     fn();
         // })
-        (this.cache[eventName]||[]).forEach(fn=>fn())
+        (this.cache[eventName] || []).forEach(fn => fn(data))
+    }
+
+    //取消订阅
+    off(eventName, fn:(data:unknown)=>void) {
+        //把缓存数组的指定的函数删除
+        this.cache[eventName] = this.cache[eventName] || []
+        // let index
+        // for (let i = 0; i < this.cache[eventName].length; i++) {
+        //     if (this.cache[eventName][i] === fn) {
+        //         index = i
+        //         break;
+        //     }
+        // }
+        // if (index === undefined){
+        //     return
+        // }else{
+        //     this.cache[eventName].splice(index,1)
+        // }
+        let index = indexOf(this.cache[eventName],fn)
+        if (index===-1) return
+        this.cache[eventName].splice(index,1)
     }
 }
 
+
 export default EventHub
 
+/**
+ * 帮助函数 indexOf
+ * @param array
+ * @param item
+ */
+function indexOf(array,item){
+    let index  = -1
+    for (let i = 0; i < array.length; i++) {
+        if (array[i]===item){
+            index = i
+            break;
+        }
+    }
+    return index
+}
--- coder/EventHub/test/index.ts
+++ coder/EventHub/test/index.ts
@@ -3,11 +3,33 @@ import EventHub from "../src";
 console.log(EventHub)
 
 const eventHub = new EventHub()
-console.log('是否是对象',eventHub instanceof Object)
+const test1=()=>{
+    console.log('是否是对象',eventHub instanceof Object)
+}
 
-//on emit
-eventHub.on("xxx",()=>{
-    console.log('被调用')
-})
+const test2=()=> {
+//emit发布 on订阅
+    eventHub.on("xxx", y => {
+        console.log('被调用')
+        console.log('y', y)
+    })
 
-eventHub.emit("xxx")
+    eventHub.emit("xxx", '接收到了吗')
+}
+
+const test3=()=> {
+    const eventHub2 = new EventHub()
+    let called2 = false;
+    const fn1 = () => {
+        called2 = true
+    }
+    eventHub2.on('yyy', fn1)
+    eventHub2.off("yyy", fn1); //取消订阅
+    eventHub2.emit("yyy")
+    setTimeout(() => {
+        console.log('called2', called2)
+    }, 1000)
+}
+test1()
+test2()
+test3()

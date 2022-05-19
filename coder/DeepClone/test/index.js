24e2975366be7cb9ce5e8dc3a5a2d4128f5d8912
liuyidiao <1070692044@qq.com>
2022-05-19 20:56:16 +0800

    浅拷贝测试用例

--- coder/DeepClone/test/index.js
+++ coder/DeepClone/test/index.js
@@ -13,7 +13,20 @@ describe('deepClone赋值',()=>{
     })
     //测试用例
     it('能够复制基本类型',()=>{
-        const n = 123;
+        //基本类型没有引用
+        const a = 123;
+        const a2 = deepClone(n)
+        assert(a===a2)
+        const s = "123456"
+        const s2 = deepClone(s)
+        assert(s===s2)
+        const t = true
+        const t2 = deepClone(t)
+        assert(t===t2)
+        const u = undefined
+        const u2 = deepClone(u)
+        assert(u===u2)
+        const n = null
         const n2 = deepClone(n)
         assert(n===n2)
     })

c01d4aac478ab53d92f6fd083f9c16089269f074
liuyidiao <1070692044@qq.com>
2022-05-19 21:10:13 +0800

    拷贝普通对象

--- coder/DeepClone/src/index.js
+++ coder/DeepClone/src/index.js
@@ -1,5 +1,13 @@
-function deepClone(source){
+function deepClone(source) {
     // return source   //浅拷贝没有引用 直接返回
+
+    if (source instanceof Object) {
+        const dist = new Object()
+        for (const sourceKey in source) {
+            dist[key] = deepClone(source[key])
+        }
+        return dist;
+    }
 }
 
 module.exports = deepClone

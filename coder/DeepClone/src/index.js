f5450381f8aacbd83326579032e02e315251a2b8
liuyidiao <1070692044@qq.com>
2022-05-21 00:14:15 +0800

    函数深拷贝进行环检测

--- coder/DeepClone/src/index.js
+++ coder/DeepClone/src/index.js
@@ -1,35 +1,43 @@
 let cache = [] //循环栈
 
 function deepClone(source) {
-
     if (source instanceof Object) {
         //是否在缓存
-        if (cache.indexOf(source) >= 0) {
-            return source
+        let cacheDist = findCache(source)
+        if (cacheDist) {
+            return cacheDist
         } else {
-            cache.push(source)
-        }
-        let dist
-        //判断是否是数组
-        if (source instanceof Array) {
-            dist = new Array();
+            let dist
+            //判断是否是数组
+            if (source instanceof Array) {
+                dist = new Array();
 
-        } else if (source instanceof Function) {
-            dist = function () {
-                return source.apply(this, arguments);
+            } else if (source instanceof Function) {
+                dist = function () {
+                    return source.apply(this, arguments);
+                }
+            } else {
+                dist = new Object()
             }
-        } else {
-            dist = new Object()
+            cache.push([source,dist])
+            for (const sourceKey in source) {
+                //递归循环
+                dist[sourceKey] = deepClone(source[sourceKey])
+            }
+            return dist;
         }
-        for (const sourceKey in source) {
-            //递归循环
-            dist[sourceKey] = deepClone(source[sourceKey])
-        }
-        return dist;
     }
     return source   //浅拷贝没有引用 直接返回
 }
 
+function findCache(source){
+    for (let i =0;i<cache.length;i++){
+        if(cache[i][0]===source){
+            return cache[i][1];
+        }
+    }
+    return  undefined
+}
 module.exports = deepClone
 
 
--- coder/DeepClone/test/index.js
+++ coder/DeepClone/test/index.js
@@ -32,39 +32,51 @@ describe('deepClone赋值', () => {
     })
 
 //    复制对象
-    describe("拷贝对象",()=>{
-        it("复制普通对象",()=>{
-            const a = {name : "张三",child:{name:"张小小"}}
+    describe("拷贝对象", () => {
+        it("复制普通对象", () => {
+            const a = {name: "张三", child: {name: "张小小"}}
             const a2 = deepClone(a);
-            assert(a!==a2)
+            assert(a !== a2)
             assert(a.name === a2.name)
             assert(a.child !== a2.child)
             assert(a.child.name === a2.child.name)
         })
-        it("复制数组对象",()=>{
-            const a = [[11,12],[21,21],[31,32]]
+        it("复制数组对象", () => {
+            const a = [[11, 12], [21, 21], [31, 32]]
             const a2 = deepClone(a);
-            assert(a!==a2)
-            assert(a[0]!==a2[0])
-            assert(a[1]!==a2[1])
-            assert(a[2]!==a2[2])
-            assert.deepEqual(a,a2)
+            assert(a !== a2)
+            assert(a[0] !== a2[0])
+            assert(a[1] !== a2[1])
+            assert(a[2] !== a2[2])
+            assert.deepEqual(a, a2)
         })
-        it("复制函数对象",()=>{
-            const a = function (x,y){return x+y}
-            a.xxx = {yyy:{zzz:1}}
+        it("复制函数对象", () => {
+            const a = function (x, y) {
+                return x + y
+            }
+            a.xxx = {yyy: {zzz: 1}}
             const a2 = deepClone(a)
-            assert(a!==a2)
-            assert(a.xxx.yyy.zzz ===a2.xxx.yyy.zzz)
-            assert(a.xxx.yyy !==a2.xxx.yyy)
-            assert(a.xxx !==a2.xxx)
-            assert(a(1,2) ===a2(1,2))
+            assert(a !== a2)
+            assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz)
+            assert(a.xxx.yyy !== a2.xxx.yyy)
+            assert(a.xxx !== a2.xxx)
+            assert(a(1, 2) === a2(1, 2))
         })
-        it("环检测，环可以用复制",()=>{
-            const a = {name:'小'};
+        it("环检测，环可以用复制", () => {
+            const a = {name: '小'};
             a.self = a
             const a2 = deepClone(a)
             assert(a === a2)
+        })
+        it("不会爆栈", () => {
+            const a = {child: null}
+            let b = a
+            for (let i = 0; i < 20000; i++) {
+                b.child = {
+                    child: null
+                }
+                b = b.child
+            }
         })
     })
 })

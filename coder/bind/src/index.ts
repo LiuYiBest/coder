45581c0881892c1358d5ba9888b5190c75a65724
liuyidiao <1070692044@qq.com>
2022-05-25 21:08:48 +0800

    bind

---
+++ coder/bind/src/index.ts
@@ -0,0 +1,3 @@
+if(!Function.prototype.bind){
+    Function.prototype.bind = function (){};
+}
---
+++ coder/bind/test/index.ts
@@ -0,0 +1,4 @@
+console.log(Function.prototype.bind)  //存在原型链上
+// Object.defineProperty(Function.prototype,"bind",{
+//
+// })

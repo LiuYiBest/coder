7beef4feb2119cd3f5f49ba10947c9ce47b11b78
liuyidiao <1070692044@qq.com>
2022-05-26 19:41:28 +0800

    bind

--- coder/bind/src/index.ts
+++ coder/bind/src/index.ts
@@ -1,3 +1,14 @@
-if(!Function.prototype.bind){
-    Function.prototype.bind = function (){};
+// if(!Function.prototype.bind){
+//     Function.prototype.bind = function (){};
+// }
+
+function bind(asThis,args){
+    //this就是函数
+    const fn = this;
+    return function (){
+        return fn.call(asThis,...args)
+    }
 }
+export default bind
+
+

ccbc0da4d179937ca56cc9fd53bb1c2cb36b5fe0
liuyidiao <1070692044@qq.com>
2022-06-06 21:21:27 +0800

    promise测试是否是一个函数

--- coder/promise/test/index.ts
+++ coder/promise/test/index.ts
@@ -1,12 +1,29 @@
 import * as chai from "chai";
 
 const assert = chai.assert
+import Promise from "../src/index";
+
 describe('Promise', () => {
     it("是一个类", () => {
         // 单元测试
+        // @ts-ignore
+        assert(typeof Promise === "function")
+        // @ts-ignore
+        assert(typeof Promise.prototype === "Object")
     });
-    // it("拥有对象方式then",()=>{
-    //
-    // });
+    // @ts-ignore
+    it("new Promise()必须接受一个函数", () => {
+        //new Promise会报错
+        throw assert(() => {
+            // @ts-ignore
+            new Promise()
+        })
+        assert.throw(() => {
+            // @ts-ignore
+            new Promise(1)
+        })
+    });
+
+
 })
 

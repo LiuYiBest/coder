a38b261ae7695a347120f691425ebff1c05c69bb
liuyidiao <1070692044@qq.com>
2022-06-06 21:20:57 +0800

    promise测试用例

--- coder/promise/src/index.ts
+++ coder/promise/src/index.ts
@@ -1,4 +1,14 @@
+class Promise2{
+    constructor(fn) {
+        if(typeof  fn !== "function"){
+            throw  new Error("只接受函数")
+        }
+        fn(()=>{},()=>{});  //给fn函数传递两个函数参数,resolve和reject
+    }
+    then(){}
+}
 
+export  default  Promise2
 
 
 

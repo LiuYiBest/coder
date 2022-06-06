5377552c27322e1098cf3f53e84cfac3b1fd2686
liuyidiao <1070692044@qq.com>
2022-06-06 21:35:47 +0800

    resolve和reject作为变量传递

--- coder/promise/src/index.ts
+++ coder/promise/src/index.ts
@@ -1,16 +1,33 @@
-class Promise2{
-    succeed =null
-    fail =null
+class Promise2 {
+    succeed = null
+    fail = null
+
+    resolve = () => {
+        setTimeout(() => {
+            this.succeed()
+        }, 0)
+    }
+    reject = () => {
+        setTimeout(() => {
+            this.fail()
+        })
+    }
+
     constructor(fn) {
-        if(typeof  fn !== "function"){
+        if (typeof fn !== "function") {
             throw  new Error("只接受函数")
         }
-        fn(()=>{},()=>{});  //给fn函数传递两个函数参数,resolve和reject
+        //给fn函数传递两个函数参数,resolve和reject
+        fn(this.resolve, this.reject);
     }
-    then(success,fail){}
+
+    then(succeed, fail) {
+        this.succeed = succeed;
+        this.fail = fail;
+    }
 }
 
-export  default  Promise2
+export default Promise2
 
 
 

c7873e8ffc1a01c82b07eed5cad9445a44f2ad22
liuyidiao <1070692044@qq.com>
2022-06-06 21:24:59 +0800

    promise测试有succeed和fail

--- coder/promise/src/index.ts
+++ coder/promise/src/index.ts
@@ -1,11 +1,13 @@
 class Promise2{
+    succeed =null
+    fail =null
     constructor(fn) {
         if(typeof  fn !== "function"){
             throw  new Error("只接受函数")
         }
         fn(()=>{},()=>{});  //给fn函数传递两个函数参数,resolve和reject
     }
-    then(){}
+    then(success,fail){}
 }
 
 export  default  Promise2

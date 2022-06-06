4b808daa26f8bc79124ebeb696f70c3b1696a0b9
liuyidiao <1070692044@qq.com>
2022-06-06 20:58:47 +0800

    promise

---
+++ coder/promise/test/index.ts
@@ -0,0 +1,12 @@
+import * as chai from "chai";
+
+const assert = chai.assert
+describe('Promise', () => {
+    it("是一个类", () => {
+        // 单元测试
+    });
+    // it("拥有对象方式then",()=>{
+    //
+    // });
+})
+
